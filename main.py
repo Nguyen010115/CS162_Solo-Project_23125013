367de8e4d131a7a909b833ebbf91082fca534110
Mai Trung Nguyen <maitrungnguyen010115@gmail.com>
2024-03-17 19:31:47 +0700

    Rebuild project

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from route_data.ClassBuild import RouteVarQuery
+from route_data.ClassBuild import StopQuery
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+query = RouteVarQuery('route_data/vars.json')
 
+#print(query.route_vars[0].getter('RouteVarName'))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#print(query.search_by_properties('RouteId', 3))
 
+#print(query.search_by_properties('Outbound', "False"))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#query.output_as_json('output_files/package.json')
+#query.output_as_csv('output_files/output.csv')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+stop_query = StopQuery('route_data/stops.json')
+print(stop_query.stops[0].datalist["RouteId"])
+print(stop_query.search_by_properties("Name", "Bến xe Chợ Lớn"))
+
+stop_query.output_as_json('output_files/output_stops.json')
+stop_query.output_as_csv('output_files/output2_stops.csv')
