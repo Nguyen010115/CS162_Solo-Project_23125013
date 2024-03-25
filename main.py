340961b39e284e08da1fbcf1195ef7b46f264077
Mai Trung Nguyen <maitrungnguyen010115@gmail.com>
2024-03-25 11:46:38 +0700

    - Xây dựng class newPath/newPath query để tái cấu trúc và hợp nhất path và stops

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-from route_data.ClassBuild import RouteVarQuery, PathQuery, StopQuery
+from route_data.ClassBuild import (RouteVarQuery, PathQuery, StopQuery, lat_lng_to_xy, calculate_distance,
+                                   calculate_distance_xy, newPath_Query)
+import geopy.distance
 
 query = RouteVarQuery('route_data/vars.json')
 
-#print(query.route_vars[0].getter('RouteVarName'))
-
-#print(query.search_by_properties('RouteId', 3))
-
-#print(query.search_by_properties('Outbound', "False"))
-
-#query.output_as_json('output_files/package.json')
-#query.output_as_csv('output_files/output.csv')
-
-stop_query = StopQuery('route_data/stops.json')
-'''print(stop_query.stop_routes[0].datalist["RouteId"])
-print(stop_query.search_by_properties("Name", "Bến xe Chợ Lớn"))
-
-stop_query.output_as_json('output_files/output_stops.json')
-stop_query.output_as_csv('output_files/output2_stops.csv')
 
 
-for ss in stop_query.stop_routes[0].get_stop_id_list:
-    print(ss["Lng"], ss["Lat"])'''
 
 path_query = PathQuery('route_data/paths.json')
-path_query.geojson_paths_write('output_files/geojson.json')
+
+#x1, y1 = lat_lng_to_xy(106.70585632, 10.77678967)
+#x2, y2 = lat_lng_to_xy(106.58197784, 10.97655869)
+#print(calculate_distance(106.70585632, 10.77678967, 106.58197784, 10.97655869))
+
+stop_query = StopQuery('route_data/stops.json')
+
+pair_stop_path = []
+
+for path in path_query.paths:
+    for stop in stop_query.stop_routes:
+        if path.get_route_id() == stop.get_route_id and path.get_route_var_id() == stop.get_route_var_id:
+            pair_stop_path.append([stop, path])
+            break
+
+new_paths = newPath_Query(pair_stop_path)
+new_paths.output_as_json('output_files/new_paths.json')
+
+
+
+
+
+
+
+
+
+
+
+
--- route_data/ClassBuild.py
+++ route_data/ClassBuild.py
@@ -1,5 +1,36 @@
 import json
 import csv
+import pyproj
+from math import radians, cos, sin, sqrt, atan2
+import heapq
+from decimal import Decimal
+
+
+def calculate_distance_xy(x1, y1, x2, y2):
+    # Using Haversine formula to calculate distance between two points
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def lat_lng_to_xy(longitude, latitude):
+    # Define the projection from geographic coordinates (lat/lon) to Cartesian (x/y)
+    projection = pyproj.Proj(proj='merc', ellps='WGS84')
+
+    # Convert lat/lon to x/y
+    x, y = projection(longitude, latitude)
+
+    return x, y
+
+
+def calculate_distance(lng1, lat1, lng2, lat2):
+    # Using Haversine formula to calculate distance between two points
+    R = 6371000  # Radius of the Earth in meters
+    lat1, lng1, lat2, lng2 = map(radians, [lat1, lng1, lat2, lng2])  # Convert degrees to radians
+    dlat = lat2 - lat1
+    dlon = lng2 - lng1
+    a = (dlat / 2) ** 2 + cos(lat1) * cos(lat2) * (dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+    return distance
 
 
 class RouteVar:
@@ -15,10 +46,10 @@ class RouteVar:
         self._distance = data_dictionary['Distance']
         self._out_bound = data_dictionary['Outbound']
         self._running_time = data_dictionary['RunningTime']
-        self.datalist = data_dictionary
+        self.data_list = data_dictionary
 
     def getter(self, properties):
-        return self.datalist[properties]
+        return self.data_list[properties]
 
     @property
     def route_id(self):
@@ -27,7 +58,7 @@ class RouteVar:
     @route_id.setter
     def route_id(self, value):
         self._route_id = value
-        self.datalist['RouteId'] = value
+        self.data_list['RouteId'] = value
 
     @property
     def route_var_id(self):
@@ -36,7 +67,7 @@ class RouteVar:
     @route_var_id.setter
     def route_var_id(self, value):
         self._route_var_id = value
-        self.datalist['RouteVarId'] = value
+        self.data_list['RouteVarId'] = value
 
     @property
     def route_var_name(self):
@@ -45,7 +76,7 @@ class RouteVar:
     @route_var_name.setter
     def route_var_name(self, value):
         self._route_var_name = value
-        self.datalist['RouteVarName'] = value
+        self.data_list['RouteVarName'] = value
 
     @property
     def route_var_short_name(self):
@@ -54,7 +85,7 @@ class RouteVar:
     @route_var_short_name.setter
     def route_var_short_name(self, value):
         self._route_var_short_name = value
-        self.datalist['RouteVarShortName'] = value
+        self.data_list['RouteVarShortName'] = value
 
     @property
     def route_no(self):
@@ -63,7 +94,7 @@ class RouteVar:
     @route_no.setter
     def route_no(self, value):
         self._route_no = value
-        self.datalist['RouteNo'] = value
+        self.data_list['RouteNo'] = value
 
     @property
     def start_stop(self):
@@ -72,7 +103,7 @@ class RouteVar:
     @start_stop.setter
     def start_stop(self, value):
         self._start_stop = value
-        self.datalist['StartStop'] = value
+        self.data_list['StartStop'] = value
 
     @property
     def end_stop(self):
@@ -81,7 +112,7 @@ class RouteVar:
     @end_stop.setter
     def end_stop(self, value):
         self._end_stop = value
-        self.datalist['EndStop'] = value
+        self.data_list['EndStop'] = value
 
     @property
     def distance(self):
@@ -90,7 +121,7 @@ class RouteVar:
     @distance.setter
     def distance(self, value):
         self._distance = value
-        self.datalist['Distance'] = value
+        self.data_list['Distance'] = value
 
     @property
     def out_bound(self):
@@ -99,7 +130,7 @@ class RouteVar:
     @out_bound.setter
     def out_bound(self, value):
         self._out_bound = value
-        self.datalist['Outbound'] = value
+        self.data_list['Outbound'] = value
 
     @property
     def running_time(self):
@@ -108,7 +139,7 @@ class RouteVar:
     @running_time.setter
     def running_time(self, value):
         self._running_time = value
-        self.datalist['RunningTime'] = value
+        self.data_list['RunningTime'] = value
 
 
 class RouteVarQuery:
@@ -130,10 +161,10 @@ class RouteVarQuery:
         data = []
         if properties in self.property_list:
             for var in self.route_vars:
-                if str(var.datalist[properties]).lower() == str(search_data).lower():
+                if str(var.data_list[properties]).lower() == str(search_data).lower():
                     data.append(
-                        ['Id: ' + var.datalist['RouteId'], 'Name: ' + var.datalist['RouteVarName'],
-                         'Short Name' + var.datalist['RouteVarShortName']])
+                        ['Id: ' + var.data_list['RouteId'], 'Name: ' + var.data_list['RouteVarName'],
+                         'Short Name' + var.data_list['RouteVarShortName']])
             return data
         else:
             print("There is no property found")
@@ -142,7 +173,7 @@ class RouteVarQuery:
         try:
             with open(file_name, 'w', encoding='utf8') as jsonfile:
                 for route_var in self.route_vars:
-                    json.dump(route_var.datalist, jsonfile, ensure_ascii=False)
+                    json.dump(route_var.data_list, jsonfile, ensure_ascii=False)
                     jsonfile.write('\n')
         except Exception as e:
             print("An error occurred while opening the file:", e)
@@ -151,19 +182,15 @@ class RouteVarQuery:
         try:
             with open(file_name, 'w', newline='', encoding='utf8') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(self.route_vars[0].datalist)
+                writer.writerow(self.route_vars[0].data_list)
                 for var in self.route_vars:
                     ss = []
-                    for com in var.datalist:
-                        ss.append(var.datalist[com])
+                    for com in var.data_list:
+                        ss.append(var.data_list[com])
                     writer.writerow(ss)
         except Exception as e:
             print("An error occurred while opening the file:", e)
 
-
-# ["RouteId", "RouteVarId", "RouteVarName", "RouteVarShortName",
-# "RouteNo", "StartStop", "EndStop", "Distance", "Outbound", "RunningTime"]
-# [var.datalist['RouteId'], var.datalist['RouteVarId'], var.datalist["RouteVarName"], var.datalist["RouteVarShortName"], var.datalist["RouteNo"], var.datalist["StartStop"], var.datalist["EndStop"], var.datalist["Distance"], var.datalist["Outbound"], var.datalist["RunningTime"]]
 
 class Stop:
     def __init__(self, data_array):
@@ -175,11 +202,17 @@ class Stop:
     def get_coordinates(self):
         coordinates = []
         for var in self._stop_id_list:
-            coordinates.append([var['Lng'], var['Lat']])
+            coordinates.append([var['Lng'], var['Lat'], var["StopId"]])
         return coordinates
 
     @property
     def get_stop_id_list(self):
+        id_list = []
+        for stop in self._stop_id_list:
+            id_list.append(stop["StopId"])
+        return id_list
+
+    def get_stops_list(self):
         return self._stop_id_list
 
     @get_stop_id_list.setter
@@ -216,6 +249,25 @@ class StopQuery:
                     self.stop_routes.append(Stop(v))
         except Exception as e:
             print("An error occurred while opening the file:", e)
+
+    def stop_id_list(self):
+        id_list = []
+        appear = set()
+        for stop in self.stop_routes:
+            for stop_id in stop.get_stops_list():
+                if stop_id["StopId"] in appear:
+                    continue
+                else:
+                    appear.add(stop_id["StopId"])
+                    id_list.append({"StopId": stop_id["StopId"], "Lng": stop_id["Lng"], "Lat": stop_id["Lat"]})
+        return id_list
+
+    def belongings(self, stop_id: int):
+        list1 = []
+        for stop in self.stop_routes:
+            if stop_id in stop.get_stop_id_list:
+                list1.append([stop.get_route_id, stop.get_route_var_id])
+        return list1
 
     def search_by_properties(self, properties, search_data):
         datas = []
@@ -286,12 +338,26 @@ class Path:
         self._lng = data['lng']
         self._route_id = data['RouteId']
         self._route_var_id = data['RouteVarId']
+        self._xy_coordinate = []
+        self._ll_coordinate = []
+        for xl, yl in zip(self._lng, self._lat):
+            x, y = lat_lng_to_xy(xl, yl)
+            self._xy_coordinate.append([x, y])
+        for lng, lat in zip(self._lng, self._lat):
+            self._ll_coordinate.append([lng, lat])
 
     def get_route_id(self):
         return self._route_id
 
+    def get_xy(self):
+        return self._xy_coordinate
+
+    def get_ll_coordinate(self):
+        return self._ll_coordinate
+
     def get_route_var_id(self):
         return self._route_var_id
+
     def get_lng(self):
         return self._lng
 
@@ -338,3 +404,102 @@ class PathQuery:
         # Write the GeoJSON data to the specified file
         with open(filename, 'w') as file:
             json.dump(geo_data, file)
+
+    '''def distance_between(self, lng, lat):
+        for path in self.paths:
+            for [lngs, lats] in path.get_ll_coordinate():
+                if lngs == lng and lats == lat:'''
+
+    def search_lng_lat(self, stop_id, stop_query: StopQuery, lng, lat):
+        result_set = []
+        for path in self.paths:
+            if [str(path.get_route_id()), str(path.get_route_var_id())] in stop_query.belongings(stop_id):
+                for i, [lng1, lat1] in enumerate(path.get_ll_coordinate()):
+                    if str(lng) == str(lng1) and str(lat) == str(lat1):
+                        result_set.append(1)
+                        result_set.append({
+                            "RouteId": path.get_route_id(),
+                            "RouteVarId": path.get_route_var_id(),
+                            "Pos": i
+                        })
+        return result_set
+
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v, dis, time):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append((v, dis, time))
+
+    def dijkstra_fastest_path(self, start):
+        distances = {vertex: float('inf') for vertex in self.graph}
+        times = {vertex: float('inf') for vertex in self.graph}
+        distances[start] = 0
+        times[start] = 0
+
+        priority_queue = [(0, start)]  # (time, vertex)
+        while priority_queue:
+            current_time, current_vertex = heapq.heappop(priority_queue)
+            if current_time > times[current_vertex]:
+                continue
+
+            for neighbor, dis, time in self.graph[current_vertex]:
+                total_time = current_time + time
+                if total_time < times[neighbor]:
+                    times[neighbor] = total_time
+                    distances[neighbor] = distances[current_vertex] + dis
+                    heapq.heappush(priority_queue, (total_time, neighbor))
+
+        return distances, times
+
+
+class newPath:
+    def __init__(self, stops: Stop, path: Path):
+        self.newPath = []
+
+        old_path = path.get_ll_coordinate()
+        temp_stop = stops.get_coordinates()
+        len_path = len(old_path)
+        len_stop = len(temp_stop)
+
+        self.newPath.append([temp_stop[0][0], temp_stop[0][1], temp_stop[0][2]])
+        cur_stop = 1
+        cur_path = 0
+
+        while cur_stop != len_stop or cur_path != len_path:
+            if cur_stop == len(temp_stop):
+                self.newPath.append([old_path[cur_path][0], old_path[cur_path][1], -1])
+                cur_path += 1
+            elif cur_path == len(old_path):
+                self.newPath.append([temp_stop[cur_stop][0], temp_stop[cur_stop][1], temp_stop[cur_stop][2]])
+                cur_stop += 1
+            else:
+                if (calculate_distance(self.newPath[-1][0], self.newPath[-1][1],
+                                       temp_stop[cur_stop][0], temp_stop[cur_stop][1])
+                        < calculate_distance(self.newPath[-1][0], self.newPath[-1][1],
+                                             old_path[cur_path][0], old_path[cur_path][1])):
+                    self.newPath.append([temp_stop[cur_stop][0], temp_stop[cur_stop][1], temp_stop[cur_stop][2]])
+                    cur_stop += 1
+                else:
+                    self.newPath.append([old_path[cur_path][0], old_path[cur_path][1], -1])
+                    cur_path += 1
+
+
+class newPath_Query:
+
+    def __init__(self, pair_stop_path: list):
+        self.newPaths = []
+        for [stop, path] in pair_stop_path:
+            self.newPaths.append(newPath(stop, path))
+
+    def output_as_json(self, file_name):
+        try:
+            with open(file_name, 'w', encoding='utf8') as jsonfile:
+                for new_path in self.newPaths:
+                    json.dump(new_path.newPath, jsonfile, ensure_ascii=False)
+                    jsonfile.write('\n')
+        except Exception as e:
+            print("An error occurred while opening the file:", e)
